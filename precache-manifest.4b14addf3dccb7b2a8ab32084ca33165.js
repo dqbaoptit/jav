0a04c3aa6e186e9500bf67f33a788735bb8d4fbc
Bao <dqbaoptit@gmail.com>
2020-05-28 11:52:51 +0700

    Updates

---
+++ precache-manifest.4b14addf3dccb7b2a8ab32084ca33165.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b3287700f21da997bdf5fe4439134dd",
+    "url": "/jav/index.html"
+  },
+  {
+    "revision": "552ddd942f72266df9d9",
+    "url": "/jav/static/css/2.63b65d4e.chunk.css"
+  },
+  {
+    "revision": "1d048df2fae7ca1167fa",
+    "url": "/jav/static/css/main.eedcf859.chunk.css"
+  },
+  {
+    "revision": "552ddd942f72266df9d9",
+    "url": "/jav/static/js/2.d6e6502c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/jav/static/js/2.d6e6502c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d048df2fae7ca1167fa",
+    "url": "/jav/static/js/main.aa2e7942.chunk.js"
+  },
+  {
+    "revision": "a3f18d7a8f83c3cb2dd1",
+    "url": "/jav/static/js/runtime-main.7bbd0ee1.js"
+  }
+]);
